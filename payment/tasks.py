cc14d27a3a49416df7366ea33f378a14832e7aa0
Elmatio <pikdzudaist@gmail.com>
2024-04-10 20:28:29 +0300

    Изменение Админ-панели

--- payment/tasks.py
+++ payment/tasks.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.conf import settings
 from orders.models import Order
+import weasyprint
 
 
 @shared_task
@@ -13,18 +14,19 @@ def payment_completed(order_id):
     successfully paid.
     """
     order = Order.objects.get(id=order_id)
-    # create invoice e-mail
-    subject = f'My Shop - Invoice no. {order.id}'
-    message = 'Please, find attached the invoice for your recent purchase.'
+    subject = f'СОМ - Счёт-фактура №  {order.id}'
+    message = 'Пожалуйста, приложите к письму счёт вашей недавней покупки'
     email = EmailMessage(subject,
                          message,
-                         'admin@myshop.com',
+                         'admin@webstore.com',
                          [order.email])
-    # generate PDF
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
     out = BytesIO()
-    # attach PDF file
+    stylesheets = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(out,
+                                           stylesheets=stylesheets)
     email.attach(f'order_{order.id}.pdf',
                  out.getvalue(),
                  'application/pdf')
-    # send e-mail
     email.send()
